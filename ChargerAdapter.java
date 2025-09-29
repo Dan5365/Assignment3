4c21dbc557f0c70f80856de66ab150837f54fdcb
Danmur <hirohelper75@gmail.com>
2025-09-29 14:23:47 +0500

    minor changes ChargerAdapter.java

--- ChargerAdapter.java
+++ ChargerAdapter.java
@@ -2,8 +2,8 @@ public class ChargerAdapter implements UsbCCharger {
     private final String chargerType;
 
     public ChargerAdapter(String html) {
-        int start = html.indexOf(">") + 1;       // после ">"
-        int end = html.indexOf("</div>");        // до конца тега
+        int start = html.indexOf(">") + 1;    
+        int end = html.indexOf("</div>");       
         this.chargerType = html.substring(start, end);
     }
 
@@ -12,3 +12,4 @@ public class ChargerAdapter implements UsbCCharger {
         return chargerType + " (adapted to USB-C)";
     }
 }
+
